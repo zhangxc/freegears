3247d40c566e5c1b24a7ff40c161ab1c2373c55a
ED. Wick <izhangxc@gmail.com>
2012-07-07 11:31:46 +0800

    Add unit test codes here

---
+++ fgtest/utest.c
@@ -0,0 +1,18 @@
+/* utest.c - unit test code.
+ *
+ * EDWick @2012-7
+ */
+#include <compat-util.h>
+#include <memwatch.h>
+
+int main(int argc, char *argv[])
+{
+    /* memwatch test codes */
+    mwInit();
+    mwStatistics( 2 );
+
+    /* unit test codes start here */
+
+
+	return 0;
+}
